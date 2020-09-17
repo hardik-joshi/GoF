59685978b8ce4efb49b978d4e74446ff6e9d2157
Hardik Joshi <joshi.hardik654@gmail.com>
2020-09-17 20:57:28 +0530

    Added Adapter pattern

---
+++ src/structural/adapter/EmployeeClient.java
@@ -0,0 +1,19 @@
+package structural.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeClient {
+    public List<Employee> getEmployeeList() {
+        List<Employee> employees = new ArrayList<>();
+        EmployeeDB employeeFromDB = new EmployeeDB("1234", "Hardik", "Joshi", "hardik@gmail.com");
+        employees.add(employeeFromDB);
+
+//        Will not work! This is where the adapter comes in play!
+//        Employee employeeFromLdap = new EmployeeLdap("2345", "Charles", "White", "charles@gmail.com");
+        EmployeeLdap employeeFromLdap = new EmployeeLdap("2345", "Charles", "White", "charles@gmail.com");
+        employees.add(new EmployeeAdapterLdap(employeeFromLdap));
+
+        return employees;
+    }
+}
